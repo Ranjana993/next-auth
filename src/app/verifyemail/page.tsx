c20d5ddf25fa597ba3f46e2a46b6c72ec144e02e
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-04-15 19:03:54 +0530

    completed

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,68 @@
+"use client"
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+
+const VerifyEmail = () => {
+  const [token, setToken] = useState("")
+  const [error, setError] = useState(false)
+  const [isVerified, setIsVerified] = useState(false)
+
+
+  const verifyEmail = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/users/verifyemail", { token })
+      setIsVerified(true)
+      console.log(token);
+      
+      console.log("Email verified successfully");
+
+    }
+    catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {
+    const urlToken = window.location.search.split("=")[1]
+    setToken(urlToken || "")
+
+  }, [])
+
+  useEffect(() => {
+    if (token.length > 0) {
+      verifyEmail()
+    }
+  }, [])
+  return (
+    <>
+      <div className='bg-black flex items-center  justify-center text-white h-screen '>
+        <div className='text-center'>
+          <h2>Verify email </h2>
+          <button className='bg-orange-500 text-black p-2 px-12'>{token ? <p>{token}</p> : "no token"}  </button>
+          {
+            isVerified && (<>
+              <h3>Verified </h3>
+              <Link href={"/sign-in"}>Login</Link>
+            </>
+
+            )
+          }
+          {
+            error && (<>
+              <h3> Error</h3>
+
+            </>
+
+            )
+          }
+        </div>
+
+      </div>
+    </>
+
+  )
+}
+
+export default VerifyEmail 
